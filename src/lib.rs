288c4c4f8224d9546403e5cd48db01471bd5fd42
mitchmindtree <mitchell.nordine@gmail.com>
2017-05-10 04:57:05 +1000

    Make functions deterministic, more efficient and more flexible

--- src/lib.rs
+++ src/lib.rs
@@ -2,72 +2,58 @@ extern crate num;
 extern crate rand;
 extern crate utils;
 
-use utils::map_range;
-use rand::{Rand, random};
-use std::cell::RefCell;
-use std::fmt::Debug;
-use num::{Float, FromPrimitive};
+/// Gen raw gaussian value with distribution mean at 0.
+pub fn gen_raw<R>(mut rng: R) -> f64
+    where R: rand::Rng,
+{
+    use rand::distributions::normal::StandardNormal;
+    let StandardNormal(f) = rng.gen();
+    f
+}
 
-/// A thread local global for holding onto the second generated value without requiring the user
-/// stores an instance of some type.
-thread_local!(static MAYBE_NEXT_VALUE: RefCell<Option<f64>> = RefCell::new(None));
+/// Generates a raw gaussian value between [0.0, 1.0) whose distribution's mean is at `value` with
+/// the given amount of `randomness` between [0.0, 1.0).
+///
+/// **Panic**s if the `value` is less than 0.0 or greater than or equal to 1.0.
+///
+/// **Panic**s if the `randomness` is less than 0.0 or greater than or equal to 1.0.
+pub fn gen<R>(mut rng: R, value: f64, randomness: f64) -> f64
+    where R: rand::Rng,
+{
+    assert!(value >= 0.0);
+    assert!(value < 1.0);
+    assert!(randomness >= 0.0);
+    assert!(randomness <= 1.0);
 
-#[inline]
-fn two<F>() -> F where F: Float { let one: F = F::one(); one + one }
+    // If there is no randomness, return the value as it was given.
+    if randomness == 0.0 {
+        return value;
+    }
 
-/// Gen raw gaussian value with dist. at 0.
-#[inline]
-pub fn gen_raw<F>() -> F where F: Float + FromPrimitive + Rand {
-    MAYBE_NEXT_VALUE.with(|maybe_next_value| {
-        let maybe_next = *maybe_next_value.borrow();
-        if let Some(next_value) = maybe_next {
-            *maybe_next_value.borrow_mut() = None;
-            FromPrimitive::from_f64(next_value).unwrap()
-        } else {
-            let (zero, one, two): (F, F, F) = (F::zero(), F::one(), two::<F>());
-            let (mut va, mut vb, mut s): (F, F, F) = (zero, zero, zero);
-            while s >= one || s == zero {
-                va = two * random::<F>() - one;
-                vb = two * random::<F>() - one;
-                s = va * vb + va * vb
-            };
-            let multi = ((-two) * s.abs().ln() / s).abs().sqrt();
-            *maybe_next_value.borrow_mut() = (vb * multi).to_f64();
-            va * multi
+    // If there is complete randomness, generate a uniform distribution value.
+    if randomness == 1.0 {
+        return rand::Rng::gen_range(&mut rng, 0.0, 1.0);
+    }
+
+    // Offset the value over the normal distributions
+    let offset_value = value * 2.0 - 1.0;
+
+    // Keep attempting values until we get one that falls within the range.
+    loop {
+        let normal = gen_raw(&mut rng);
+        let attempt = normal * randomness + offset_value;
+        if -1.0 <= attempt && attempt < 1.0 {
+            return (attempt + 1.0) * 0.5;
         }
-    })
-}
-
-/// Gen gaussian value with dist. at 'n' with rand randomness.
-/// Generated value will always be 0.0 <= value < 1.0.
-#[inline]
-pub fn gen<F>(n: F, randomness: f32) -> F
-where F: Float + Rand + FromPrimitive + Debug {
-    let (zero, one): (F, F) = (F::zero(), F::one());
-    assert!(n >= zero && n <= one, "Gaussian::gen : given `n` ({:?}) must \
-            be a percentage between 0 and 1.", n);
-
-    // If one was given with no randomness, return it exactly as is.
-    if n == one && randomness == 0.0 {
-        return one - F::epsilon();
-    }
-
-    let mut ans = gen_raw::<F>()
-                * FromPrimitive::from_f32(randomness.powf(2.0)).unwrap()
-                + (n * two::<F>() - one);
-    ans = map_range(ans, -one, one, zero, one);
-    if ans >= one || ans < zero {
-        gen::<F>(n, randomness)
-    } else {
-        ans
     }
 }
 
-/// Gen gaussian value mapped to a range.
-#[inline]
-pub fn gen_map<F>(n: F, randomness: f32, min_range: F, max_range: F) -> F
-where F: Float + Rand + FromPrimitive + Debug {
-    let (zero, one): (F, F) = (F::zero(), F::one());
-    let perc = map_range(n, min_range, max_range, zero, one);
-    map_range(gen(perc, randomness), zero, one, min_range, max_range)
+/// Generate a gaussian value mapped to the given range [min, max).
+pub fn gen_map_range<R, T>(rng: R, value: T, randomness: f64, min: T, max: T) -> T
+    where R: rand::Rng,
+          T: num::NumCast + Copy,
+{
+    let f = utils::map_range(value, min, max, 0.0, 1.0);
+    let r = gen(rng, f, randomness);
+    utils::map_range(r, 0.0, 1.0, min, max)
 }
--- tests/gen.rs
+++ tests/gen.rs
@@ -1,19 +1,26 @@
-
 extern crate gaussian;
 extern crate rand;
 
+use rand::Rng;
+
 #[test]
 pub fn gen() {
+    let mut rng = rand::thread_rng();
     for _ in 0..1_000_000 {
-        let v = gaussian::gen(::rand::random::<f32>(), ::rand::random::<f32>());
+        let f = rng.next_f64();
+        let r = rng.next_f64();
+        let v = gaussian::gen(&mut rng, f, r);
         assert!(v >= 0.0 && v < 1.0);
     }
 }
 
 #[test]
 pub fn gen_map() {
+    let mut rng = rand::thread_rng();
     for _ in 0..1_000_000 {
-        let v = gaussian::gen_map(::rand::random::<f32>(), ::rand::random::<f32>(), -50.0, 100.0);
+        let f = rng.next_f64();
+        let r = rng.next_f64();
+        let v = gaussian::gen_map_range(&mut rng, f, r, -50.0, 100.0);
         assert!(v >= -50.0 && v < 100.0);
     }
 }
